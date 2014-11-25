28f4f2e7efd996ac786c07ea8f5c7df3ed741d09
John Thomason <jahwffrey@snoopy.cs.ucsb.edu>
2014-11-24 16:47:00 -0800

    can write to a ppm

---
+++ Makefile
@@ -0,0 +1,10 @@
+all: prog
+
+prog: prog.cpp
+	g++ -o prog4 main.cpp -std=c++11
+	echo "****Programming Assignment 4 for CS180 by John Thomason****"
+	./prog4
+	display testimg.ppm
+	
+clean:
+	rm -rf *.o prog4
---
+++ main.cpp
@@ -0,0 +1,20 @@
+//John Thomason Assignment 4 Main File
+#include <fstream>
+
+int main(){
+	std::ofstream stream;
+	stream.open("testimg.ppm");
+	
+	int width = 255;
+	int height = 255;
+
+	stream <<  "P1\n" << width << " " << height << "\n255\n";
+	for(int y = 0; y < height; y++){
+		for(int x = 0; x < width; x++){
+			stream << "0 ";
+		}
+		stream << "\n";
+	}
+
+	return 0;
+}
